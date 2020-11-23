85e7ec746702d54c51a2e145a9aeb8e10f7998ee
ancientTexts-net <zach.pennington@unfoldingWord.org>
2020-11-23 14:19:06 -0600

    MarkdownContext

--- src/components/datatable/DataTable.js
+++ src/components/datatable/DataTable.js
@@ -1,3 +1,8 @@
+import { Toolbar } from '../..';
+import { getMuiTheme } from './muiTheme';
+import { DataTableContext, DataTableContextProvider } from './DataTable.context';
+import { getColumns, getData } from './helpers';
+
 import React, {
   useState, useContext, useRef, useCallback, useMemo,
 } from 'react';
@@ -6,18 +11,19 @@ import useEffect from 'use-deep-compare-effect';
 import PropTypes from 'prop-types';
 import MUIDataTable from 'mui-datatables';
 import { MuiThemeProvider } from '@material-ui/core/styles';
-import { Toolbar } from '../..';
-import { getMuiTheme } from './muiTheme';
-import { DataTableContext, DataTableContextProvider } from './DataTable.context';
-import { getColumns, getData } from './helpers';
+
+import { MarkdownContext, MarkdownContextProvider } from 'markdown-translatable';
+
 const fixedHeaderOptions = { xAxis: false, yAxis: false };
 const rowsPerPageOptions = [25, 50, 100];
 
 export default function DataTableWrapper(props) {
   return (
-    <DataTableContextProvider {...props}>
-      <DataTable {...props} />
-    </DataTableContextProvider>
+    <MarkdownContextProvider>
+      <DataTableContextProvider {...props}>
+        <DataTable {...props} />
+      </DataTableContextProvider>
+    </MarkdownContextProvider>
   );
 }
 
@@ -28,8 +34,8 @@ const DatatableMemo = React.memo(function ({
   return (<MUIDataTable ref={dataTableElement} columns={columns} options={options} data={data} />);
 }, (prevProps, nextProps) => {
   const equal = isEqual(prevProps.data, nextProps.data) &&
-  isEqual(prevProps.columns, nextProps.columns) &&
-  isEqual(prevProps.options, nextProps.options);
+    isEqual(prevProps.columns, nextProps.columns) &&
+    isEqual(prevProps.options, nextProps.options);
   return equal;
 });
 
@@ -55,7 +61,9 @@ function DataTable({
   const {
     columnNames, data, changed, columnsFilterOptions,
   } = state;
-  const { cellEdit:_cellEdit } = actions;
+  const { cellEdit: _cellEdit } = actions;
+
+  const { state: markdownState, actions: markdownActions } = useContext(MarkdownContext);
 
   const generateRowId = useCallback(_generateRowId, []);
 
@@ -74,7 +82,11 @@ function DataTable({
   const _onSave = useCallback(() => {
     const savedFile = actions.targetFileSave();
     onSave(savedFile);
-  }, [actions, onSave]);
+
+    if (markdownActions && markdownActions.setIsChanged) {
+      markdownActions.setIsChanged(false);
+    }
+  }, [actions, onSave, markdownActions]);
 
   const onColumnViewChange = useCallback((changedColumn, action) => {
     let _columnsShow = [...columnsShow];
@@ -98,7 +110,9 @@ function DataTable({
     scrollToTop();
   }, [scrollToTop]);
 
-  const customToolbar = useCallback(() => <Toolbar preview={preview} onPreview={togglePreview} changed={changed} onSave={_onSave} />, [_onSave, changed, preview, togglePreview]);
+  const customToolbar = useCallback(() => <Toolbar preview={preview} onPreview={togglePreview} changed={changed || markdownState.isChanged} onSave={_onSave} />
+    , [_onSave, changed, markdownState.isChanged, preview, togglePreview]);
+
   const _options = useMemo(() => ({
     responsive: 'scrollFullHeight',
     fixedHeaderOptions,
@@ -114,7 +128,7 @@ function DataTable({
     print: false,
     customToolbar,
     ...options,
-  }),[customToolbar, onChangeRowsPerPage, onColumnViewChange, options, rowsPerPage, scrollToTop] );
+  }), [customToolbar, onChangeRowsPerPage, onColumnViewChange, options, rowsPerPage, scrollToTop]);
 
   const _data = useMemo(() => getData({
     data, columnNames, rowHeader,
