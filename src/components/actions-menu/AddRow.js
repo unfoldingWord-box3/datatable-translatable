811bdffe990f7666b6b60b10d2e50f9bf179d2e4
mandolyte <cecil.new@gmail.com>
2022-06-01 11:19:28 -0400

    Working for Spt Ref

--- src/components/actions-menu/AddRow.js
+++ src/components/actions-menu/AddRow.js
@@ -9,17 +9,21 @@ import {
   DialogActions,
   DialogContent,
   DialogContentText,
+  TextField,
 } from '@material-ui/core';
 import {
 } from '@material-ui/icons';
+import Autocomplete, { createFilterOptions } from '@material-ui/lab/Autocomplete';
 import { getRowElement, getOffset } from '../../core/datatable';
 import { DataTableContext } from '../datatable/DataTable.context';
 
+const filter = createFilterOptions();
 
 function AddRowMenu({
   rowData,
   rowIndex,
   columnNames,
+  dropdownColumn = 4,
   rowGenerate,
   rowAddBelow,
   button,
@@ -27,9 +31,10 @@ function AddRowMenu({
 }) {
   const [open, setOpen] = useState(false);
   const [newRow, setNewRow] = useState();
+  const [value, setValue] = useState('');
 
   const { state } = useContext(DataTableContext);
-  console.log("columnfilteroptions:", state.columnsFilterOptions);
+  // console.log("Datatable Context state:", state);
 
   const classes = useStyles();
 
@@ -41,6 +46,7 @@ function AddRowMenu({
   };
 
   const handleRowAdd = () => {
+    newRow[dropdownColumn] = value.name ? value.name : value;
     rowAddBelow({ rowIndex, rowData: newRow });
     handleClose();
     setTimeout(() => {
@@ -60,22 +66,60 @@ function AddRowMenu({
     const newRowComponent = columnNames.map((name, i) => {
       let text = '';
 
+
       if (!newRow) {
         const _newRow = rowGenerate({ rowIndex });
         setNewRow(_newRow);
         return text;
       } else {
-        console.log("newRowComponent() name,i=",name,i)
-        if ( state.columnsFilterOptions[i] ) {
-          if ( state.columnsFilterOptions[i].length > 0 )
-          console.log("Boom! put an autocomplete here")
-        }
+        // by default...
         text = (
           <DialogContentText key={name + i}>
             <strong>{name}:</strong>
             {' ' + newRow[i]}
           </DialogContentText>
         );
+        if ( i === dropdownColumn && state.columnsFilterOptions[i] ) {
+          if ( state.columnsFilterOptions[i].length > 0 ) {
+            const defaultProps = {
+              options: state.columnsFilterOptions[i].map(
+                (value, index) => {
+                  return { id: index, name: value}
+                }
+              ),
+              getOptionLabel: (option) => option.name ? option.name : value,
+              getOptionSelected: (option, val) => {
+                return option.name === val.name;
+              }
+            };
+            text = (
+              <Autocomplete
+                {...defaultProps}
+                id="spt-ref"
+                value={value}
+                onChange={(event, newValue) => {
+                  // console.log("Autocomplete() onchange() setValue:", newValue)
+                  setValue(newValue);
+                }}
+                renderInput={(params) => <TextField {...params} label={state.columnNames[i]} margin="normal" />}
+                freeSolo={true}
+                onBlur
+                filterOptions={(options, params) => {
+                  const filtered = filter(options, params);
+                  // Suggest the creation of a new value
+                  if (params.inputValue !== '') {
+                    filtered.push({
+                      id: params.id,
+                      name: `${params.inputValue}`,
+                    });
+                  }
+          
+                  return filtered;
+                }}
+              />
+            );        
+          }
+        }
       }
       return text;
     });
