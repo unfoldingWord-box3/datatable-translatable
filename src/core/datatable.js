90f19b2cb129894df8c279b16080287995cc792e
Joel C Johnson <joelcjohnson123@gmail.com>
2022-06-22 18:27:04 +0530

    removed the multiple sorting from the code

--- src/core/datatable.js
+++ src/core/datatable.js
@@ -180,9 +180,6 @@ export const getColumnsFilterOptions = ({
       });
       if (_columnsFilterOptions[columnIndex]) {
         _columnsFilterOptions[columnIndex].sort();
-        _columnsFilterOptions[columnIndex].sort(function(a, b) {
-          return a - b;
-        });
       }
     });
   });
